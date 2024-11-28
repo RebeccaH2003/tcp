bc317d99e17fdba0c896242c49e7747bf89ba7f6
Rebecca Huang <huan2859@iits-b473-20.utsc-labs.utoronto.ca>
2024-11-28 15:03:00 -0500

    update

--- tcp_helper.c
+++ tcp_helper.c
@@ -42,10 +42,11 @@ void stop_and_wait(int sockfd, uint32_t packet_size, uint64_t duration) {
             uint32_t ack_seq_num;
             
             ssize_t recv_status = recv(sockfd, &ack_seq_num, sizeof(ack_seq_num), 0);
-            printf("get the receive, ack_seq_num: %u \n", ack_seq_num);
+            
             if (recv_status > 0) {
                 // Convert ACK sequence number from network byte order
                 ack_seq_num = ntohl(ack_seq_num);
+                printf("get the receive, ack_seq_num: %u \n", ack_seq_num);
 
                 if (ack_seq_num == sequence_number) {
                     printf("received successfully!\n");
